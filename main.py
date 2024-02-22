8683a26bab916481da8d118a3b3c133d49c80e1d
stabbyfork <markus2018mmh@gmail.com>
2024-02-22 12:40:36 +0200

    testing

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import re
 
 readFile = open("./testconv.py")
 writeFile = open("./converted.cpp", "w")
+lines = readFile.readlines()
+writeLines = []
 
 tabNumber = 0
 classDict = {}
+filePointer = 1
+
+def lineWrite(text, writeLines=writeLines, filePointer=filePointer, tabNumber=tabNumber):
+    if re.findall(";\n", text) == []:
+        text = text + ";\n"
+    print(filePointer)
+    print(tabNumber)
+    text = (tabNumber * 4) * " " + text
+    writeLines.insert(filePointer, text)
 
 # layer/stage 1: modifications for conversion
-if re.findall("class main():", readFile.read()) == None:
-    writeFile.write("int main() {\n")
-    classDict.update(main = 'int')
-    print(classDict)
+def fixClass(findClass="main", filePointer=filePointer, tabNumber=tabNumber):
+    for line in lines:
+        if re.findall("class main():", line) == "class main():":
+            return True
+    writeLines.append("int main() {\n")
+    filePointer += 1
     tabNumber += 1
+    lineWrite("int num = 5;\n")
+    classDict.update(main = 'int')
+    writeLines.append("}\n")
+    print(classDict)
+    return False
+
+fixClass()
+
+writeFile.writelines(writeLines)
+readFile.close()
+writeFile.close()
